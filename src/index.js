be98946486bbb3638d17b3c4f3962998ef95e244
avelbryl <avelticoalu22@gmail.com>
2024-04-22 19:42:16 +0800

    add/course

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+// // middleware untuk menangani request body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const courseController = require("./course/course.controller");
+
+app.use("/courses", courseController);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
