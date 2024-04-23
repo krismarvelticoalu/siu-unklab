f3d636ffe870d547b4d00aa62d960f579672b4a0
avelbryl <avelticoalu22@gmail.com>
2024-04-23 16:07:21 +0800

    add pg for database connection and empty the course.controller.js also add get and post methods in index.js entry point

--- codes/src/index.js
+++ codes/src/index.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("../db");
 
 // // middleware untuk menangani request body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const courseController = require("./course/course.controller");
+app.get("/courses", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM mata_kuliah");
+    res.status(200).json({
+      status: "success",
+      data: result.rows,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-app.use("/courses", courseController);
+app.post("/courses", async (req, res) => {
+  const { id_mk, nama_mk, ruangan, paralel } = req.body;
+  try {
+    const result = await db.query(
+      `INSERT into mata_kuliah (id_mk, nama_mk, ruangan, paralel) values ('${id_mk}', '${nama_mk}', '${ruangan}', '${paralel}')`
+    );
+    res.status(200).json({
+      status: "success",
+      message: "data berhasil dimasukan",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
