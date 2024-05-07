deb8bd8ed524cd5d6e775ee7483e6a0690b64fac
avelbryl <avelticoalu22@gmail.com>
2024-05-07 14:33:20 +0800

    add student code

--- Back-End/codes/src/index.js
+++ Back-End/codes/src/index.js
@@ -8,8 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const courseController = require("./course/course.controller");
+const studentController = require("./student/student.controller");
 
 app.use("/courses", courseController);
+app.use("/students", studentController);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
