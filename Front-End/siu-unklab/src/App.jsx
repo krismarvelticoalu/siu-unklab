d50e4c53773954e43ec2689d5c8a3c7c2c913d70
avelbryl <avelticoalu22@gmail.com>
2024-05-13 02:50:17 +0800

    edit directoriy for image

--- Front-End/siu-unklab/src/App.jsx
+++ Front-End/siu-unklab/src/App.jsx
@@ -1,20 +1,27 @@
-
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
     <div className="login-container">
       <div className="login-form">
         <div className="logo-container">
-          <img src="./" alt="Logo" className="logo" />
+          <img src="images\logo-.png" alt="Logo" className="logo" />
         </div>
-        <input type="text" placeholder="Registration Number" className="input-field" />
+        <input
+          type="text"
+          placeholder="Registration Number"
+          className="input-field"
+        />
         <input type="password" placeholder="Password" className="input-field" />
-        <a href="home.jsx"><button className="login-button">Login</button></a>
-        <a href="#" className="forgot-password">Reset Session?</a>
+        <a href="home.jsx">
+          <button className="login-button">Login</button>
+        </a>
+        <a href="#" className="forgot-password">
+          Reset Session?
+        </a>
       </div>
     </div>
   );
 };
 
-export default App
+export default App;
